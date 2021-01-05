b3b59547032afb304899a707c9c346c310a1043f
amirmohammad pirhosseinloo <amir.hosseinloo@cafebazaar.ir>
2021-01-06 03:08:23 +0330

    Map job submission view, job status view and job upload result view to urls

--- panel/urls.py
+++ panel/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import upload_jar
+from .views import create_list_job, retrieve_job, upload_partial_result
 
 urlpatterns = [
-    path('upload_jar/', upload_jar, name='upload_jar'),
+    path('jobs/', create_list_job, name='create_list_job'),
+    path('jobs/<uuid:id>', retrieve_job, name='retrieve_job'),
+    path('jobs/<uuid:id>/partial-results/', upload_partial_result, name='create_partial_result'),
 ]
