b72bbeb613b2a63682fccbaa5c619a94c73bfa3e
amirmohammad pirhosseinloo <amir.hosseinloo@cafebazaar.ir>
2021-01-06 03:07:46 +0330

    Redesign job submission view, job status view and job result upload view

--- panel/views.py
+++ panel/views.py
@@ -1,19 +1,63 @@
 from django.http.response import HttpResponse
-from panel.models import JarFile
-from panel.forms import JarFileForm
 from django.shortcuts import render
-from django.template import RequestContext
-from django.http import HttpResponseRedirect
+from django.conf import settings
+from django.contrib.auth.decorators import login_required
+from django.views.decorators.csrf import csrf_exempt
+from panel.models import Job, JobPartialResult
+from state_manager import submit_job, complete_inprogress_job
 
 
-def upload_jar(request):
+@login_required
+def create_list_job(request):
     if request.method == 'POST':
-        form = JarFileForm(request.POST, request.FILES)
-        if form.is_valid():
-            new_jar = JarFile(jarfile=request.FILES['jarfile'])
-            new_jar.save()
-            return HttpResponse("ok")
-    else:
-        form = JarFileForm()
+        try:
+            new_job = Job.objects.create(
+                user=request.user, executable=request.FILES['executable'], input_file=request.FILES['input_file'])
+            submit_job(new_job)
+        except KeyError:
+            return HttpResponse(content="bad request", status=400)
+        except Exception as e:
+            print(str(e))
+            return HttpResponse(content="internal server error", status=503)
+        return HttpResponse(content="job submitted successfully", status=201)
+    elif request.method == 'GET':
+        jobs = Job.objects.filter(user=request.user)
+        return render(request, 'index.html', {'jobs': jobs})
+    return HttpResponse(content='method not allowed', status=405)
 
-    return render(request, 'upload_jar.html')
+
+@login_required
+def retrieve_job(request, id):
+    if request.method == 'GET':
+        try:
+            job = Job.objects.get(user=request.user, id=id)
+            partial_results = JobPartialResult.objects.filter(job=job)
+        except Job.DoesNotExist:
+            return HttpResponse(content='job not found', status=404)
+        return render(request, "job.html", context={'executable': job.executable,
+                                                    'input_file': job.input_file,
+                                                    'partial_results': partial_results
+                                                    })
+    return HttpResponse(content='method not allowed', status=405)
+
+
+@csrf_exempt
+def upload_partial_result(request, id):
+    if request.method == 'POST':
+        try:
+            # job = Job.objects.get(user=request.user, id=id) TODO make it secure
+            job = Job.objects.get(id=id)
+            JobPartialResult.objects.create(job=job, index=request.POST['index'],
+                                            partial_result_file=request.FILES['file'])
+            device_id = request.POST['device_id']
+            try:
+                complete_inprogress_job(device_id)
+            except (TypeError, KeyError, AssertionError) as e:
+                print(str(e))
+                return HttpResponse(content='internal server error', status=503)
+            return HttpResponse(content='uploaded successfully', status=201)
+        except Job.DoesNotExist:
+            return HttpResponse(content='job not found', status=404)
+        except KeyError:
+            return HttpResponse(content='bad request', status=400)
+    return HttpResponse(content='method not allowed', status=405)
